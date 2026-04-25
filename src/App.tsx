18c381ce05f87fb5ae24220d8440489a299b90ec
yellowrevolver22 <akshitk275@gmail.com>
2026-04-25 18:54:28 +0530

    started projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css'
+import logoImage from '/PETERSONS-logo.png'
 
 function App() {
   return (
-    <div className=''>
-      
+    <div className='flex justify-between items-center px-10 text-xl'>
+      <div className='w-30'>
+        <img className='w-full h-full object-cover' src={logoImage}></img>
+      </div>
+      <div className='flex gap-5'>
+        <span>NEW & FEATURED</span>
+        <span>BAGS</span>
+        <span>SLINGS</span>
+        <span>ACCESSORIES</span>
+        <span>SHOES</span>
+      </div>
+      <div>
+        icons
+      </div>
     </div>
   )
 }
