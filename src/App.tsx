904e4dd70fe389835ee4e8f642d106e4bf577f14
yellowrevolver22 <akshitk275@gmail.com>
2026-04-26 20:02:07 +0530

    added navbar and custom classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
+import { BaggageClaim, Search, User } from 'lucide-react'
 import './App.css'
 import logoImage from '/PETERSONS-logo.png'
 
 function App() {
   return (
     <div className='flex justify-between items-center px-10 text-xl'>
-      <div className='w-30'>
+      <div className='w-24'>
         <img className='w-full h-full object-cover' src={logoImage}></img>
       </div>
+      <div className='flex gap-5 font-playfair'>
+        <span className='navItems navItems:hover'>NEW & FEATURED</span>
+        <span className='navItems navItems:hover'>SUITS</span>
+        <span className='navItems navItems:hover'>ACCESSORIES</span>
+        <span className='navItems navItems:hover'>SHOES</span>
+      </div> 
       <div className='flex gap-5'>
-        <span>NEW & FEATURED</span>
-        <span>BAGS</span>
-        <span>SLINGS</span>
-        <span>ACCESSORIES</span>
-        <span>SHOES</span>
-      </div>
-      <div>
-        icons
+        <span className='cursor-pointer'><Search/></span>
+        <span className='cursor-pointer'><User/></span>
+        <span className='cursor-pointer'><BaggageClaim/></span>
       </div>
     </div>
   )
